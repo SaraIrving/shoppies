97e13d41437ff85dc7bc73a9806ed3feac98abf2
Sara <sarakirving@gmail.com>
2021-01-13 19:49:37 -0800

    refactor to include form element, write functions to handle changes to the value in teh textarea and to handle submission of the form, now that we can isolate the final value the users inputs we can use this to make the api call

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 
 
-export default function SearchBox() {
+export default function SearchBox(props) {
+
+  const changeHandler = (event) => {
+    console.log("In change handler, event = ", event.target.value)
+    props.setState(prev => ({...prev, searchValue: event.target.value}))
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    // trigger api call now with final product that is pulled out of state?
+    console.log("IN submission handler")
+    console.log(props.state.searchValue)
+  }
 
 
   return (
 
     <div>
       <p>This is the search box</p>
-      <textarea ></textarea>
+      <form onSubmit={submitHandler}>
+        <label></label>
+        <textarea name="searchBox" type="text" onChange={changeHandler}></textarea>
+        <button type="submit">Search</button>
+      </form>
+    
 
     </div>
   )
