d359f47d0f77bb71b2d08913865f233408255afb
Sara <sarakirving@gmail.com>
2021-01-13 19:30:50 -0800

    create SearchBox component which will hold teh textarea where users can input their movies searches, next build out onChange feature so that the inputted value is captured and can be used to make an api call

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default function SearchBox() {
+
+
+  return (
+
+    <div>
+      <p>This is the search box</p>
+      <textarea ></textarea>
+
+    </div>
+  )
+
+}
