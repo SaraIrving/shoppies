b778b99970eb673ff30c303e09d48b912d18c008
Sara <sarakirving@gmail.com>
2021-01-13 20:24:01 -0800

    pull searchValue out of state and use it to make the api call to get the top 5 movie results, then use the results to update the results array in state

--- src/hooks/useAppData.js
+++ src/hooks/useAppData.js
@@ -1,13 +1,23 @@
 import { useState, useEffect } from 'react';
-//import axios from 'axios';
+import axios from 'axios';
 
 
 export default function useAppData() {
 
   const [state, setState] = useState({
-                                      searchValue: 'initial',
+                                      enteredValue: 'initial',
+                                      searchValue: '',
                                       resultsArray: []
                                     });
+
+  useEffect(() => {
+    axios.get(`http://www.omdbapi.com/?apikey=bb6449b7&s=${state.searchValue}`)
+    .then((response) => {
+      console.log("response.data  = ", response.data.Search)
+      //now use the data to set the results list in the state!
+    })
+
+  }, [state.searchValue])
 
   return{
     state,
