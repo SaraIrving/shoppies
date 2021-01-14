22d82e84cbc9e83592a2905561452c362b6e37f0
Sara <sarakirving@gmail.com>
2021-01-14 10:38:15 -0800

    create Button component which will take in a classname, onClick function and text to display as props

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function (props) {
+
+
+  return (
+    <button className={props.className} onClick={props.onClick}>{props.buttonLabel}</button>
+  );
+}
