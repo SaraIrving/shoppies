aaa0b3fcafb5f8d1388ee4c2e7ad6d79597306b0
Sara <sarakirving@gmail.com>
2021-01-15 15:41:31 -0800

    add a property to state - enableDragDrop - which will be a boolean indicating if the drag and drop feature is active

--- src/hooks/useAppData.js
+++ src/hooks/useAppData.js
@@ -8,7 +8,8 @@ export default function useAppData() {
                                       enteredValue: 'initial',
                                       searchValue: '',
                                       resultsArray: [],
-                                      nominationsArray: []
+                                      nominationsArray: [],
+                                      enableDragDrop: false
                                     });
 
   useEffect(() => {
