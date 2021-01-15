807428c9a126c7a0c21ef582f28991dccc31c4b5
Sara <sarakirving@gmail.com>
2021-01-14 17:23:58 -0800

    create the determineButtonDisable function which will return a boolean based on whether or not the Movie component in question is already present in the NominationList, if so we want to remove it's nomination button so it cannot be nominated twice - this boolean value will be used as the disable prop being passed to the Movie component

--- src/components/ResultsList.jsx
+++ src/components/ResultsList.jsx
@@ -23,8 +23,23 @@ export default function(props) {
                         title: movie.Title,
                         year: movie.Year
                         };
+
+        // if a result is already in the nominations list, don't give it a Nominate button
+        const determineButtonDisable = function(nomArray) {
+        
+          for (let nom of nomArray) {
+            if (nom.title === movieObj.title){
+              return true;
+            } 
+          }
+          return false;
+        };
+
+        const disableProp = determineButtonDisable(props.state.nominationsArray);
+
+
         return(
-          <Movie movie={movieObj} key={index} state={props.state} setState={props.setState} buttonLabel="Nominate" submitHandler={getResultsSubmitHandler(movieObj)}></Movie>
+          <Movie disable={disableProp} movie={movieObj} key={index} state={props.state} setState={props.setState} buttonLabel="Nominate" submitHandler={getResultsSubmitHandler(movieObj)}></Movie>
         );
       })}
     </div>}
