3f2844b2daf7606a5ac9a71dad610929acba9347
Sara <sarakirving@gmail.com>
2021-01-14 16:57:50 -0800

    refactor the functions which will be passed as the submission handler to the Movie component, have it take in the key as an argument and then return the event handler function that the Movie component will need

--- src/components/NominationsList.jsx
+++ src/components/NominationsList.jsx
@@ -3,14 +3,30 @@ import Movie from './Movie';
 
 export default function NominationsList(props) {
 
+  // submission handler for the form in the Movie component
+
+  const getNomSubmitHandler = function(key){
+    return ((event) => {
+        event.preventDefault();
+        // remove the movie in question from the nominations array
+        
+        props.setState(prev => {
+          const nomCopy = prev.nominationsArray
+          nomCopy.splice(key, 1);
+          return(
+            {...prev, nominationsArray: nomCopy}
+          )
+        })
+      }
+    )
+  }
+
   return (
     <div className="nominationsList">
       {props.state.nominationsArray.length !== 0 && <div>
         {props.state.nominationsArray.map((movie, index) => {
-          console.log("NOM MOVIE = ", movie);
-          console.log("NOM MOVIE props = ", props);
           return (
-            <Movie movie={movie} key={index} state={props.state} setState={props.setState} buttonLabel="Remove"></Movie>
+            <Movie movie={movie} key={index} state={props.state} setState={props.setState} buttonLabel="Remove" submitHandler={getNomSubmitHandler(index)}></Movie>
           );
         })}
       </div>}
