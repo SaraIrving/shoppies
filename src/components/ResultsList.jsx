0778d0207089506dbd8e5a065f012d3ac493fb0a
Sara <sarakirving@gmail.com>
2021-01-14 16:58:42 -0800

    refactor the function which will be passed as a submission handler to the Movie component, have it take in the movie object as an argument and return the event handler that Movie will need

--- src/components/ResultsList.jsx
+++ src/components/ResultsList.jsx
@@ -3,6 +3,17 @@ import Movie from './Movie';
 
 export default function(props) {
 
+  // // submission handler for the form in the Movie component
+
+  const getResultsSubmitHandler = function(movie) {
+    return ((event) => {
+      event.preventDefault();
+     
+      //Add the movie in question to the nominationsArray in state
+      props.setState(prev => ({...prev, nominationsArray: [...prev.nominationsArray, movie]}))
+    });
+  }
+
 
   return (
     <div className="resultsList">
@@ -12,9 +23,8 @@ export default function(props) {
                         title: movie.Title,
                         year: movie.Year
                         };
-
         return(
-          <Movie movie={movieObj} key={index} state={props.state} setState={props.setState} buttonLabel="Nominate"></Movie>
+          <Movie movie={movieObj} key={index} state={props.state} setState={props.setState} buttonLabel="Nominate" submitHandler={getResultsSubmitHandler(movieObj)}></Movie>
         );
       })}
     </div>}
