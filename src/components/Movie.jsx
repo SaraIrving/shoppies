4d07f1da7cf0058ca55bcc0c52ff6555551cb6dc
Sara <sarakirving@gmail.com>
2021-01-15 19:26:51 -0800

    refactor to wrap the Movie component in the Draggable component when enableDragDrop in state is true, provided necessary props

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from './Button';
+import { Draggable } from "react-beautiful-dnd";
 
 export default function Movie(props) {
 
@@ -14,15 +15,34 @@ export default function Movie(props) {
 
   return (
     <div>
-      {props.disable === true &&  <div>
-                                      <p>{props.movie.title} ({props.movie.year})</p>
+      {props.state.enableDragDrop &&  <Draggable kye={props.key} draggableId={props.draggableId} index={props.index}>
+        {(provided) => (
+          <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+          {props.disable === true &&  <div>
+                                          <p>{props.movie.title} ({props.movie.year})</p>
+                                          </div>}
+          {props.disable === false && <div>
+                                        <p>{props.movie.title} ({props.movie.year})</p>
+                                        <form onSubmit={props.submitHandler}>
+                                          <Button buttonLabel={props.buttonLabel}></Button>
+                                        </form>
                                       </div>}
-      {props.disable === false && <div>
-                                    <p>{props.movie.title} ({props.movie.year})</p>
-                                    <form onSubmit={props.submitHandler}>
-                                      <Button buttonLabel={props.buttonLabel}></Button>
-                                    </form>
-                                  </div>}
+      </div>
+        )}
+                                        
+                                      </Draggable> }
+      {!props.state.enableDragDrop && <div>
+                                        {props.disable === true &&  <div>
+                                                                        <p>{props.movie.title} ({props.movie.year})</p>
+                                                                        </div>}
+                                        {props.disable === false && <div>
+                                                                      <p>{props.movie.title} ({props.movie.year})</p>
+                                                                      <form onSubmit={props.submitHandler}>
+                                                                        <Button buttonLabel={props.buttonLabel}></Button>
+                                                                      </form>
+                                                                    </div>}
+                                        </div>}
+ 
     </div>
  
   );
