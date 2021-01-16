ae95ce23e42f811758d51117e65d237007823afc
Sara <sarakirving@gmail.com>
2021-01-15 16:50:58 -0800

    add radio button that will toggle on and off the drag and drop functionality, display elements depending on the value of the enableDragDrop property in state, if it's true display the two lists wrapped in the DragDropContext component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import useAppData from './hooks/useAppData';
 import ResultsList from './components/ResultsList';
 import NominationsList from './components/NominationsList';
 import Bounce from 'react-reveal/Bounce';
+import { DragDropContext } from "react-beautiful-dnd";
 
 function App() {
 
@@ -19,17 +20,25 @@ function App() {
       <div>
         <label>Enable Drag and drop feature to rank nominees
         </label>
-        <input type="radio" value="DNDRadio" checked={state.enableDragDrop} value="DND" onClick={event => setState(prev => ({...prev, enableDragDrop: !prev.enableDragDrop}))}></input>
-
+        <input type="radio" value="DNDRadio" checked={state.enableDragDrop} readOnly={true} onClick={event => setState(prev => ({...prev, enableDragDrop: !prev.enableDragDrop}))}></input>
       </div>
       
       {/* {state.enteredValue !== "initial" && <p>Search value in state = {state.enteredValue}</p>}
       {state.searchValue !== "" && <p>Search value in state = {state.searchValue}</p>} */}
       {state.nominationsArray.length >= 5 && <Bounce><p>NOminations list is full! Remove some if you want to nominate more</p></Bounce>}
-      <div className="resultNomWrapper">
-        <ResultsList state={state} setState={setState}></ResultsList>
-        <NominationsList state={state} setState={setState}></NominationsList>
-      </div>
+
+
+      {!state.enableDragDrop && <div className="resultNomWrapper">
+                                  <ResultsList state={state} setState={setState}></ResultsList>
+                                  <NominationsList state={state} setState={setState}></NominationsList>
+                                </div>}
+      {state.enableDragDrop && <DragDropContext>
+                                  <p>drag and drop happening</p>
+                                  <div className="resultNomWrapper">
+                                    <ResultsList state={state} setState={setState} droppableId="results" listItemType="results"></ResultsList>
+                                    <NominationsList state={state} setState={setState} droppableId="nominations" listItemType="nominations"></NominationsList>
+                                  </div>
+                                </DragDropContext>}
     </div>
    
   );
