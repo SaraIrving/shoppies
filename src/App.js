c95e905b7b8594f64dbbb87114cdf57e7bc5061b
Sara <sarakirving@gmail.com>
2021-01-16 16:16:51 -0800

    in progress refactor of handleOnDragEnd function, currently on hold to address styling - add classNames and elements for styling purposes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import useAppData from './hooks/useAppData';
 import ResultsList from './components/ResultsList';
 import NominationsList from './components/NominationsList';
 import Bounce from 'react-reveal/Bounce';
+import Flash from 'react-reveal/Flash';
 import { DragDropContext } from "react-beautiful-dnd";
 
 function App() {
@@ -14,21 +15,26 @@ function App() {
   console.log("state = ", state);
 
   const handleOnDragEnd = function(result) {
+
+    console.log("RESULT DRAG END  = ", result);
+
     //if you try to drag an element outside of the DragDropContext
     if(!result.destination) {
+      console.log("Cant drop here!")
       return;
     }
 
     // if you are drag and dropping within the results list
     if (result.destination.droppableId === "results" && result.source.droppableId === "results") {
-      const items = Array.from(state.resultsArray);
+      // const items = Array.from(state.resultsArray);
+      const items = state.resultsArray.slice(0);
       const [reorderedItem] = items.splice(result.source.index, 1);
       items.splice(result.destination.index, 0, reorderedItem);
       setState(prev => {return {...prev, resultsArray: items}})
 
     // if you are drag and dropping within the nominations list
     } else if (result.destination.droppableId === "nominations" && result.source.droppableId === "nominations") {
-      const items = Array.from(state.nominationsArray);
+      const items = Array.from(state.nominationsArray.slice(0));
       const [reorderedItem] = items.splice(result.source.index, 1);
       items.splice(result.destination.index, 0, reorderedItem);
       setState(prev => {return {...prev, nominationsArray: items}})
@@ -36,11 +42,41 @@ function App() {
     // if you are drag and dropping from results to nominations
     } else if (result.destination.droppableId !== result.source.droppableId) {
       console.log("you're switching LISTS!!")
+      console.log("source - ", result.source.droppableId)
+      console.log("destination - ", result.destination.droppableId)
       if (result.destination.droppableId === "nominations") {
-        const items = Array.from(state.nominationsArray);
+        console.log("putting it in NOMS")
+        // const items = Array.from(state.nominationsArray);
+        // destination has a droppableID and an index
+        // source has a droppableId and an index 
+        // leverage these to update state
+          // cut the dragged movie out of results state and add to nom state
+
+        // crate a copy of nom state array  
+        const items = state.nominationsArray.slice(0);
+
+        // pull out moved item from results state 
         const [reorderedItem] = state.resultsArray.splice(result.source.index, 1);
+
+        // put moved item into copy of nom state array
         items.splice(result.destination.index, 0, reorderedItem)
-        setState(prev => {return {...prev, nominationsArray: items}})
+        console.log("UPDATED noms = ", items);
+
+        // now make a copy of the results state before the removed item
+        const newResutls1 = state.resultsArray.slice(0, result.source.index);
+        console.log("Newresults1 = ", newResutls1)
+        const newResutls2 = state.resultsArray.slice(result.source.index);
+        console.log("newResults2 = ", newResutls2);
+        const updatedResults =newResutls1.concat(newResutls2);
+        console.log("updatedResults = ", updatedResults)
+
+
+        // update both nom and results state 
+        setState(prev => {return {...prev, nominationsArray: items, resultsArray: updatedResults}})
+
+
+      } else if (result.destination.droppableId === "results") {
+        console.log("Youre putting it in RESULTS")
       }
     }
 
@@ -48,9 +84,9 @@ function App() {
 
   return (
     <div className="App">
-      <p>Shoppies App</p>
+      <Flash><p>The Shoppie Awards</p></Flash>
       <SearchBox state={state} setState={setState}></SearchBox>
-      <div>
+      <div className="radioWrapper">
         <label>Enable Drag and drop feature to rank nominees
         </label>
         <input type="radio" value="DNDRadio" checked={state.enableDragDrop} readOnly={true} onClick={event => setState(prev => ({...prev, enableDragDrop: !prev.enableDragDrop}))}></input>
