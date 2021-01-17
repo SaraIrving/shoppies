091aa40010282002b5f63985fe43e20f69ce8302
Sara <sarakirving@gmail.com>
2021-01-17 14:45:24 -0800

    create Footer component to hold a quote at the bottom of the app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function () {
+
+  return (
+    <footer>
+      <p>"The Shoppie Awards has been a really healthy outlet for my judgemental tendencies" - Karen, Shoppie judge since 2021</p>
+    </footer>
+  );
+}
