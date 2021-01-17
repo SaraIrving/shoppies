3b81f6838b4fcbb01f2b643b4c6cb8a98d9bbce0
Sara <sarakirving@gmail.com>
2021-01-17 15:17:59 -0800

    refactor so that text changes depending on the enableDragDrop property of state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Flash from 'react-reveal/Flash';
 
-export default function () {
+export default function (props) {
 
-
+  console.log("Props in header = ", props);
   return (
     <header>
       <Flash><h2 className="title">The Shoppie Awards</h2></Flash>
-      <p>Search for your fav movies. Judge them with some help from your fav memes!</p>
+      {props.state.enableDragDrop &&  <p>Search for your fav movies. Judge them with some help from your fav memes!</p>}
+      {!props.state.enableDragDrop &&  <p>Search for your fav movies. Judge them with only with buttons and not pop culture references!</p>}
+     
     </header>
   )
 
