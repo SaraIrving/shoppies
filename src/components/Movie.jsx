3ff3725baaf4ec3f4ae316ef63df9becdc281be1
Sara <sarakirving@gmail.com>
2021-01-17 12:47:19 -0800

    import in meme images from images file, create an array with images and captions, have these applied to each movie in the nominations list based on that components index, need to add logic so this is only happening in the nom list as it is currently also happening in the resutls list

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Button from './Button';
 import { Draggable } from "react-beautiful-dnd";
+import cat from '../images/cryingCat.png';
+import madLady from '../images/yelling.png';
+import drakeNo from '../images/drakeNo.png';
+import drakeYes from '../images/drakeYes.png';
+import successKid from '../images/successKid.png';
 
 export default function Movie(props) {
 
@@ -11,6 +16,14 @@ export default function Movie(props) {
   // Year: "2011"
   // imdbID: "tt1568911"}
 
+  const determineImage = function(index) {
+    if(index === 0) {
+      return 
+    }
+  }
+
+  const memeArray = [[drakeYes, "First! You know what that means..."], [successKid, "Second! We're almost the best!"], [cat, "Third! Not bad, but some mixed feelings."], [madLady, "Fourth!?! I'd like to speak to the manager!"], [drakeNo, "Fifth! Pshssh!"]];
+
 
 
   return (
@@ -18,27 +31,34 @@ export default function Movie(props) {
       {props.state.enableDragDrop &&  <Draggable key={props.draggableId} draggableId={props.draggableId} index={props.index}>
         {(provided) => (
           <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-          {props.disable === true &&  <div>
+          {props.disable === true &&  <div className="movieWrapper">
                                           <p>{props.movie.title} ({props.movie.year})</p>
                                           </div>}
-          {props.disable === false && <div>
-                                        <p>{props.movie.title} ({props.movie.year})</p>
-                                        <form onSubmit={props.submitHandler}>
-                                          <Button buttonLabel={props.buttonLabel}></Button>
-                                        </form>
+          {props.disable === false && <div className="movieWrapper">
+                                        <div className="memeWrapper">
+                                          <img src={memeArray[props.index][0]} className="memeImg"/>
+                                          <p>{memeArray[props.index][1]}</p> 
+                                        </div>
+                                        <div className="movieInfoWrapper">
+                                          <p>{props.movie.title} ({props.movie.year})</p>
+                                          <form onSubmit={props.submitHandler}>
+                                            <Button buttonLabel={props.buttonLabel} className={props.className}></Button>
+                                          </form>
+                                        </div>
+                                      
                                       </div>}
       </div>
         )}
                                         
-                                      </Draggable> }
+                                      </Draggable>}
       {!props.state.enableDragDrop && <div>
-                                        {props.disable === true &&  <div>
+                                        {props.disable === true &&  <div className="movieWrapper">
                                                                         <p>{props.movie.title} ({props.movie.year})</p>
                                                                         </div>}
-                                        {props.disable === false && <div>
+                                        {props.disable === false && <div className="movieWrapper">
                                                                       <p>{props.movie.title} ({props.movie.year})</p>
                                                                       <form onSubmit={props.submitHandler}>
-                                                                        <Button buttonLabel={props.buttonLabel}></Button>
+                                                                        <Button buttonLabel={props.buttonLabel} className={props.className}></Button>
                                                                       </form>
                                                                     </div>}
                                         </div>}
