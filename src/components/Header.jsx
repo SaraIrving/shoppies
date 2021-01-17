182f4999796abfce468e4881a4828ab39e5b5b50
Sara <sarakirving@gmail.com>
2021-01-17 14:33:01 -0800

    create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Flash from 'react-reveal/Flash';
+
+export default function () {
+
+
+  return (
+    <header>
+      <Flash><h2 className="title">The Shoppie Awards</h2></Flash>
+      <p>Search for your fav movies. Judge them with some help from your fav memes!</p>
+    </header>
+  )
+
+
+}
