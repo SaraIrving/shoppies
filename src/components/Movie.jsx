95110c97a57eed4ce7122fb6858c80214dbfb324
Sara <sarakirving@gmail.com>
2021-01-17 18:30:49 -0800

    refactor conditional display of Movie components so that the results list is no longer a Droppable area even when the enableDragDrop property of state is true

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -29,7 +29,23 @@ export default function Movie(props) {
 
   return (
     <div>
-      {props.state.enableDragDrop &&  <Draggable key={props.draggableId} draggableId={props.draggableId} index={props.index}>
+       {(props.state.enableDragDrop && props.source === "results") && <div>
+                                        {props.disable === true &&  <div className="movieWrapper">
+                                                                      <div className="movieInfoWrapper">
+                                                                        <p>{props.movie.title} ({props.movie.year})</p>
+                                                                      </div>
+                                                                        </div>}
+                                        {props.disable === false && <div className="movieWrapper">
+                                                                      <div className="movieInfoWrapper">
+                                                                        <p>{props.movie.title} ({props.movie.year})</p>
+                                                                        <form onSubmit={props.submitHandler}>
+                                                                          <Button buttonLabel={props.buttonLabel} className={props.className}></Button>
+                                                                        </form>
+                                                                      </div>
+                                                                    </div>}
+                                        </div>}
+
+      {(props.state.enableDragDrop && !props.source) &&  <Draggable key={props.draggableId} draggableId={props.draggableId} index={props.index}>
         {(provided) => (
           <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
           {props.disable === true &&  <div className="movieWrapper">
@@ -62,6 +78,7 @@ export default function Movie(props) {
         )}
                                         
                                       </Draggable>}
+
       {!props.state.enableDragDrop && <div>
                                         {props.disable === true &&  <div className="movieWrapper">
                                                                       <div className="movieInfoWrapper">
