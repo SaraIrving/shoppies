efec011ae31191812024984e811d6e9e8c5e8024
Sara <sarakirving@gmail.com>
2021-01-17 18:15:50 -0800

    refactor to accept state as a prop, use to conditionally render the grumpy cat image

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -3,16 +3,16 @@ import Bounce from 'react-reveal/Bounce';
 import grumpyCat from '../images/grumpyCat.png'
 
 
-export default function Alert () {
+export default function Alert (props) {
 
   return(
     <Bounce>
       <div className="alert">
-        <img src={grumpyCat} className="memeImg"/>
+        {props.state.enableDragDrop && <img src={grumpyCat} className="memeImg"/>}
         <p>
           Nominations list is full! <br></br> Remove the duds if you want to nominate some more
         </p>
-        <img src={grumpyCat} className="memeImg"/>
+        {props.state.enableDragDrop && <img src={grumpyCat} className="memeImg"/>}
       </div>
       
     </Bounce>
