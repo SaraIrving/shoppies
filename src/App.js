b895bccc70f505a83de66ad7524b6c938755fbda
Sara <sarakirving@gmail.com>
2021-01-17 18:29:43 -0800

    refactor handleOnDragEnd function so that it is only relevant for dragging and dropping inside the nominations list now that we are scaling back where we apply the drag and drop features

--- src/App.js
+++ src/App.js
@@ -27,61 +27,68 @@ function App() {
       return;
     }
 
-    // if you are drag and dropping within the results list
-    if (result.destination.droppableId === "results" && result.source.droppableId === "results") {
-      // const items = Array.from(state.resultsArray);
-      const items = state.resultsArray.slice(0);
-      const [reorderedItem] = items.splice(result.source.index, 1);
-      items.splice(result.destination.index, 0, reorderedItem);
-      setState(prev => {return {...prev, resultsArray: items}})
-
-    // if you are drag and dropping within the nominations list
-    } else if (result.destination.droppableId === "nominations" && result.source.droppableId === "nominations") {
+    if (result.destination.droppableId === "nominations" && result.source.droppableId === "nominations") {
       const items = Array.from(state.nominationsArray.slice(0));
       const [reorderedItem] = items.splice(result.source.index, 1);
       items.splice(result.destination.index, 0, reorderedItem);
       setState(prev => {return {...prev, nominationsArray: items}})
-
-    // if you are drag and dropping from results to nominations
-    } else if (result.destination.droppableId !== result.source.droppableId) {
-      console.log("you're switching LISTS!!")
-      console.log("source - ", result.source.droppableId)
-      console.log("destination - ", result.destination.droppableId)
-      if (result.destination.droppableId === "nominations") {
-        console.log("putting it in NOMS")
-        // const items = Array.from(state.nominationsArray);
-        // destination has a droppableID and an index
-        // source has a droppableId and an index 
-        // leverage these to update state
-          // cut the dragged movie out of results state and add to nom state
-
-        // crate a copy of nom state array  
-        const items = state.nominationsArray.slice(0);
-
-        // pull out moved item from results state 
-        const [reorderedItem] = state.resultsArray.splice(result.source.index, 1);
-
-        // put moved item into copy of nom state array
-        items.splice(result.destination.index, 0, reorderedItem)
-        console.log("UPDATED noms = ", items);
-
-        // now make a copy of the results state before the removed item
-        const newResutls1 = state.resultsArray.slice(0, result.source.index);
-        console.log("Newresults1 = ", newResutls1)
-        const newResutls2 = state.resultsArray.slice(result.source.index);
-        console.log("newResults2 = ", newResutls2);
-        const updatedResults =newResutls1.concat(newResutls2);
-        console.log("updatedResults = ", updatedResults)
-
-
-        // update both nom and results state 
-        setState(prev => {return {...prev, nominationsArray: items, resultsArray: updatedResults}})
-
-
-      } else if (result.destination.droppableId === "results") {
-        console.log("Youre putting it in RESULTS")
-      }
     }
+
+    // // if you are drag and dropping within the results list
+    // if (result.destination.droppableId === "results" && result.source.droppableId === "results") {
+    //   // const items = Array.from(state.resultsArray);
+    //   const items = state.resultsArray.slice(0);
+    //   const [reorderedItem] = items.splice(result.source.index, 1);
+    //   items.splice(result.destination.index, 0, reorderedItem);
+    //   setState(prev => {return {...prev, resultsArray: items}})
+
+    // // if you are drag and dropping within the nominations list
+    // } else if (result.destination.droppableId === "nominations" && result.source.droppableId === "nominations") {
+    //   const items = Array.from(state.nominationsArray.slice(0));
+    //   const [reorderedItem] = items.splice(result.source.index, 1);
+    //   items.splice(result.destination.index, 0, reorderedItem);
+    //   setState(prev => {return {...prev, nominationsArray: items}})
+
+    // // if you are drag and dropping from results to nominations
+    // } else if (result.destination.droppableId !== result.source.droppableId) {
+    //   console.log("you're switching LISTS!!")
+    //   console.log("source - ", result.source.droppableId)
+    //   console.log("destination - ", result.destination.droppableId)
+    //   if (result.destination.droppableId === "nominations") {
+    //     console.log("putting it in NOMS")
+    //     // const items = Array.from(state.nominationsArray);
+    //     // destination has a droppableID and an index
+    //     // source has a droppableId and an index 
+    //     // leverage these to update state
+    //       // cut the dragged movie out of results state and add to nom state
+
+    //     // crate a copy of nom state array  
+    //     const items = state.nominationsArray.slice(0);
+
+    //     // pull out moved item from results state 
+    //     const [reorderedItem] = state.resultsArray.splice(result.source.index, 1);
+
+    //     // put moved item into copy of nom state array
+    //     items.splice(result.destination.index, 0, reorderedItem)
+    //     console.log("UPDATED noms = ", items);
+
+    //     // now make a copy of the results state before the removed item
+    //     const newResutls1 = state.resultsArray.slice(0, result.source.index);
+    //     console.log("Newresults1 = ", newResutls1)
+    //     const newResutls2 = state.resultsArray.slice(result.source.index);
+    //     console.log("newResults2 = ", newResutls2);
+    //     const updatedResults =newResutls1.concat(newResutls2);
+    //     console.log("updatedResults = ", updatedResults)
+
+
+    //     // update both nom and results state 
+    //     setState(prev => {return {...prev, nominationsArray: items, resultsArray: updatedResults}})
+
+
+    //   } else if (result.destination.droppableId === "results") {
+    //     console.log("Youre putting it in RESULTS")
+    //   }
+    // }
 
   };
 
