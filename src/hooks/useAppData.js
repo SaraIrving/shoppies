ee6d2fcc01fbcf47dcff8e62cceff81d46b449ac
Sara <sarakirving@gmail.com>
2021-01-17 20:57:59 -0800

    remove stray console log statements

--- src/hooks/useAppData.js
+++ src/hooks/useAppData.js
@@ -15,12 +15,10 @@ export default function useAppData() {
   useEffect(() => {
     axios.get(`https://www.omdbapi.com/?apikey=bb6449b7&type=movie&s=${state.searchValue}`)
     .then((response) => {
-      console.log("response.data  = ", response.data.Search)
       //now use the data to set the results list in the state!
 
       //isolate the first 5 responses
       const results = response.data.Search.slice(0, 5)
-      console.log(results)
       setState(prev => ({...prev, resultsArray: results}));
     })
     .catch(error => console.log(error));
